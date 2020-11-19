8aaaa5d747a439a60d86f6a0f9b7c9782d98fe33
FranciscoOssian <ossian.neto@gmail.com>
2020-11-19 10:34:20 -0300

    bug styles fixed

--- pages/Components/Bulb/index.js
+++ pages/Components/Bulb/index.js
@@ -1,3 +1,5 @@
+import styles from '../../../styles/global.css';
+
 export default function Home() {
   return (
     <div className={styles.bulb}>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import styles from '../styles/global.css';
 
 export default function Home() {
   return (
